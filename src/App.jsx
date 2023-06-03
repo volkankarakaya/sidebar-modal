36954108c297a8ae2dd9acdbe0cff6fccb3137f2
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-06-03 04:14:22 +0300

    create global context

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import Home from "./Home";
+import Sidebar from "./Sidebar"
+import Modal from "./Modal"
+
 const App = () => {
-  return <h2>Sidebar Starter</h2>;
+  return <main>
+    <Home/>
+    <Sidebar/>
+    <Modal/>
+  </main>;
 };
 export default App;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useGlobalContext } from './context'
 
 const Home = () => {
+  const {openSidebar,openModal} = useGlobalContext()
+  console.log(openSidebar);
   return (
     <div>Home</div>
   )
